71bfab17fc4e4862147a85199eec38f2a09ec53b
Kosa103 <dotomiro2@gmail.com>
2020-12-28 09:46:05 +0100

    cleaning app: shifting responsibilities

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
 import './App.css';
 import React from "react";
 
-function FormField(props) {
-  const title = props.title;
-  const id = title.split("] ")[1].toLowerCase().replace(" ", "-").replace(":", "");
 
+function ContentWelcomePage() {
   return (
-    <div className="form-field">
-      <p>{title}</p>
-      <input type="text" size="40" maxLength="40" className="input form-field" id={`input-${id}`} />
+    <div className="content-box welcome-page">
+      <h3>Welcome to Online Vehicle Database!</h3>
+      <p>Dear visitor! You can search for vehicles by fields in the vehicle's registration book. Please log in to add, modify or delete existing vehicles! Signing up is restricted for security reasons. Contact us for more information.</p>
     </div>
   );
 }
 
-function VehicleForm() {
+
+function ContentSearchVehicles(props) {
+  const searchParameterOptions = ["[A] Registration number", "[B] Date of first registration", "[D.1] Brand", "[D.2] Type", "[D.3] Model", "[P.1] Displacement",
+  "[P.5] Engine code", "[0] Eurotax code", "[P.2] Engine power", "[E] Chassis number", "[P.3] Fuel type", "[J] Vehicle category", "[G] Empty Weight",
+  "[F.1] Maximum Weight", "[K] Type acknowledgement number", "[I] Date of first registration in hungary", "[00] Year of manufacturing", "[R] Color",
+  "[V.9] Environmental class", "[S.1] Number of seats", "[C.1.1] Last name or company name", "[C.1.2] First name", "[C.1.3] Address", "[C.1.4] Ownership type"];
+
+  function renderSearchOptions(options) {
+    const optionFields = options.map((option, idx) => {
+      return (
+        <option key={`key-${option}`}>{option}</option>
+      );
+    });
+    return optionFields;
+  }
+
   return (
-    <div className="form-box">
+    <div>
+      <div className="search-field-box">
+        <p>Search by: </p>
+        <select>
+          {renderSearchOptions(searchParameterOptions)}
+        </select>
+      </div>
+      <div className="search-field-box">
+        <input type="text" size="40" maxLength="40" className="input search-field" id="search-field-vehicle" />
+        <button className="button search" id="search-button-vehicle">SEARCH</button>
+      </div>
+    </div>
+  );
+}
+
+
+function ContentAddVehicles() {
+  return (
+    <div className="content-box form-box">
       <div className="form-page">
         <div className="form-subpage">
           <FormField title="[A] Registration number:" />
@@ -63,79 +94,60 @@ function VehicleForm() {
   );
 }
 
-function OptionsBar() {
+
+function FormField(props) {
+  const title = props.title;
+  const id = title.split("] ")[1].toLowerCase().replace(" ", "-").replace(":", "");
+
+  return (
+    <div className="form-field">
+      <p>{title}</p>
+      <input type="text" size="40" maxLength="40" className="input form-field" id={`input-${id}`} />
+    </div>
+  );
+}
+
+
+function OptionsBar(props) {
+  const changeContent = props.changeContent;
+
   return (
     <div className="options-bar-box">
-      <ul className="options-bar-list">
-        <li className="button options">Search Vehicle</li>
-        <li className="button options inactive">Add Vehicle</li>
-      </ul>
+      <button className="button options-button" onClick={() => changeContent("ContentSearchVehicles")}>Search Vehicles</button>
+      <button className="button options-button inactive">Add Vehicles</button>
     </div>
   );
 }
 
-function SearchBox() {
-  return (
-    <div>
-      <button className="button search-by" id="search-by-button-vehicle">Search by: </button>
-      <div className="search-field-box">
-        <input type="text" size="40" maxLength="40" className="input search-field" id="search-field-vehicle" />
-        <button className="button search" id="search-button-vehicle"></button>
-      </div>
-    </div>
-  );
-}
 
-function LoginBox(props) {
-  const loggedIn = props.loggedIn;
-  const userName = props.userName;
-  const loginError = props.loginError;
-  const loginName = props.loginFields.loginName;
-  const loginPassword = props.loginFields.loginPassword;
-  const attemptLogin = props.attemptLogin;
-  const attemptLogout = props.attemptLogout;
-
-  function renderLoginBox() {
-    if (loggedIn) {
-      return (
-        <>
-          <p>Logged in as:</p>
-          <p>{userName}</p>
-          <button
-            className="button logout-button"
-            id="logout-button"
-            onClick={() => attemptLogout()}>
-              Logout
-          </button>
-        </>
-      );
-    } else {
-      return (
-        <>
-          <h3>Login</h3>
-          <p>Email:</p>
-          <input type="email" size="30" maxLength="30" autoComplete="off" className="input login-name" id="login-name" ref={loginName}/>
-          <p>Password:</p>
-          <input type="password" size="30" maxLength="30" autoComplete="off" className="input login-password" id="login-password" ref={loginPassword}/>
-          <p id="login-failed-message">Login failed!</p>
-          <button
-            className="button login-button"
-            id="login-submit-button"
-            onClick={() => attemptLogin()}>
-              Login
-          </button>
-        </>
-      );
-    }
+function LoginBoxAuth(props) {
+  const changeAuth = props.changeAuth;
+ 
+  function attemptLogout() {
+    sessionStorage.removeItem("res");
+    changeAuth(false);
   }
 
-  React.useEffect(() => {
-    if (loginError) {
-      document.getElementById("login-failed-message").style.visibility = "visible";
-    } else {
-      document.getElementById("login-failed-message").style.visibility = "hidden";
+  function renderLoginBox() {
+    const userData = sessionStorage.getItem("res");
+    let userName = "";
+
+    if (userData !== null) {
+      userName = JSON.parse(userData).user.username;
     }
-  }, [loginError]);
+    return (
+      <>
+        <p>Logged in as:</p>
+        <p>{userName}</p>
+        <button
+          className="button logout-button"
+          id="logout-button"
+          onClick={() => attemptLogout()}>
+            Logout
+        </button>
+      </>
+    );
+  }
 
   return (
     <div className="login-box">
@@ -144,32 +156,24 @@ function LoginBox(props) {
   );
 }
 
-function App() {
-  const searchParameters = ["[A] Registration number", "[B] Date of first registration", "[D.1] Brand", "[D.2] Type", "[D.3] Model", "[P.1] Displacement",
-  "[P.5] Engine code", "[0] Eurotax code", "[P.2] Engine power", "[E] Chassis number", "[P.3] Fuel type", "[J] Vehicle category", "[G] Empty Weight",
-  "[F.1] Maximum Weight", "[K] Type acknowledgement number", "[I] Date of first registration in hungary", "[00] Year of manufacturing", "[R] Color",
-  "[V.9] Environmental class", "[S.1] Number of seats", "[C.1.1] Last name or company name", "[C.1.2] First name", "[C.1.3] Address", "[C.1.4] Ownership type"];
 
-  const [loggedIn, setLoggedIn] = React.useState(false);
-  const [loginError, setLoginError] = React.useState(false);
-  const [userName, setUserName] = React.useState("");
+function LoginBoxUnauth(props) {
+  const changeAuth = props.changeAuth;
   
-  const loginName = React.useRef(null);
+  const [loginError, setLoginError] = React.useState(false);
+  
+  const loginEmail = React.useRef(null);
   const loginPassword = React.useRef(null);
 
   async function attemptLogin() {
-    const loginNameValue = loginName.current.value;
+    const loginEmailValue = loginEmail.current.value;
     const loginPasswordValue = loginPassword.current.value;
 
-
-    if (loginNameValue.length < 1 || loginPasswordValue.length < 1) {
+    if (loginEmailValue.length < 1 || loginPasswordValue.length < 1) {
       attemptLogout();
     } else {
 
-      const loginData = {
-        identifier: loginNameValue,
-        password: loginPasswordValue
-      };
+      const loginData = { identifier: loginEmailValue, password: loginPasswordValue };
       const url = "http://borzalom.ddns.net:1000/auth/local";
       const options = {
         method: "POST",
@@ -178,7 +182,6 @@ function App() {
         },
         body: JSON.stringify(loginData)
       };
-
       let responseStatus = 0;
       
       const response = await fetch(url, options)
@@ -190,45 +193,80 @@ function App() {
 
       if (responseStatus >= 200 && responseStatus < 300) {
         try {
-          sessionStorage.setItem("tkn", response.jwt);
-          sessionStorage.setItem("uname", response.user.userName);
-          setUserName(response.user.username);
-          setLoggedIn(true);
+          sessionStorage.setItem("res", JSON.stringify(response));
+          changeAuth(true);
           setLoginError(false);
         } catch(err) {
-          console.log(err);
           attemptLogout();
+          console.log(err);
         }
       } else {
-        attemptLogout();
+        sessionStorage.removeItem("res");
+        changeAuth(false);
+        setLoginError(true);
       }
     }
   }
 
   function attemptLogout() {
-    sessionStorage.removeItem("tkn");
-    sessionStorage.removeItem("uname");
-    setUserName("");
-    setLoggedIn(false);
+    sessionStorage.removeItem("res");
+    changeAuth(false);
     setLoginError(false);
   }
 
   React.useEffect(() => {
-    if (sessionStorage.getItem("tkn") !== null && sessionStorage.getItem("uname") !== null) {
-      setUserName(sessionStorage.getItem("uname"));
-      setLoggedIn(true);
-      setLoginError(false);
-    } else {
-      attemptLogout();
+    if (document.getElementById("login-failed-message") !== null) {
+      if (loginError) {
+        document.getElementById("login-failed-message").style.visibility = "visible";
+      } else {
+        document.getElementById("login-failed-message").style.visibility = "hidden";
+      }
     }
-  });
+  }, [loginError]);
 
+  return (
+    <div className="login-box">
+      <h3>Login</h3>
+      <p>Email:</p>
+      <input type="email" size="30" maxLength="30" autoComplete="off" className="input login-email" id="login-email" ref={loginEmail}/>
+      <p>Password:</p>
+      <input type="password" size="30" maxLength="30" autoComplete="off" className="input login-password" id="login-password" ref={loginPassword}/>
+      <p id="login-failed-message">Login failed!</p>
+      <button
+        className="button login-button"
+        id="login-submit-button"
+        onClick={() => attemptLogin()}>
+        Login
+      </button>
+    </div>
+  );
+}
+
+
+function AppAuth(props) {
+  const changeAuth = props.changeAuth;
+
+  const [displayedContent, setDisplayedContent] = React.useState("ContentWelcomePage");
+
+  const authenticated = true;
+
+  function changeContent(currentContent) {
+    setDisplayedContent(currentContent);
+  }
+
+  function renderContent() {
+    if (displayedContent === "ContentWelcomePage") {
+      return <ContentWelcomePage />;
+    } else if (displayedContent === "ContentSearchVehicles") {
+      return <ContentSearchVehicles />;
+    }
+  }
 
   return (
     <div className=".container-fluid">
       <div className="app-main-box">
         <div className="title-box">
-          <h1 className="main-title">Vehicle Database</h1>
+          <h1 className="main-title">Online Vehicle Database</h1>
         </div>
         <div className="main-box">
           <div className="left-box">
@@ -242,19 +280,15 @@ function App() {
             </div>
           </div>
           <div className="center-box">
-            <OptionsBar />
+            <OptionsBar changeContent={changeContent} />
             <div className="content-box">
-              <h2>CONTENT</h2>
+              {renderContent()}
             </div>
           </div>
           <div className="right-box">
-            <LoginBox 
-              loggedIn={loggedIn}
-              userName={userName}
-              loginError={loginError} 
-              attemptLogin={attemptLogin}
-              attemptLogout={attemptLogout}
-              loginFields={{loginName: loginName, loginPassword: loginPassword}}
+            <LoginBoxAuth 
+              authenticated={authenticated}
+              changeAuth={changeAuth}
             />
             <div className="right-content-box">
               <h3>Right Box Content 1</h3>
@@ -266,5 +300,83 @@ function App() {
     </div>
   );
 }
+
+
+function AppUnauth(props) {
+  const changeAuth = props.changeAuth;
+
+  const [displayedContent, setDisplayedContent] = React.useState("ContentWelcomePage");
+
+  const authenticated = false;
+
+  function changeContent(currentContent) {
+    setDisplayedContent(currentContent);
+  }
+
+  function renderContent() {
+    if (displayedContent === "ContentWelcomePage") {
+      return <ContentWelcomePage />;
+    } else if (displayedContent === "ContentSearchVehicles") {
+      return <ContentSearchVehicles />;
+    }
+  }
+
+  return (
+    <div className=".container-fluid">
+      <div className="app-main-box">
+        <div className="title-box">
+          <h1 className="main-title">Online Vehicle Database</h1>
+        </div>
+        <div className="main-box">
+          <div className="left-box">
+            <div className="left-content-box">
+              <h3>Left Box Content 1</h3>
+              <p>placeholder content</p>
+            </div>
+            <div className="left-content-box">
+              <h3>Left Box Content 2</h3>
+              <p>placeholder content</p>
+            </div>
+          </div>
+          <div className="center-box">
+            <OptionsBar changeContent={changeContent} />
+            <div className="content-box">
+              {renderContent()}
+            </div>
+          </div>
+          <div className="right-box">
+            <LoginBoxUnauth 
+              authenticated={authenticated}
+              changeAuth={changeAuth}
+            />
+            <div className="right-content-box">
+              <h3>Right Box Content 1</h3>
+              <p>placeholder content</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+function App() {
+  const [authenticated, setAuthenticated] = React.useState(false);
+
+  React.useEffect(() => {
+    if (sessionStorage.getItem("res") !== null) {
+      const userData = JSON.parse(sessionStorage.getItem("res"));
+      if (userData.jwt !== null) {
+        setAuthenticated(true);
+      }
+    }
+  }, []);
+
+  return (authenticated ? 
+  <AppAuth changeAuth={bool => setAuthenticated(bool)}/> : 
+  <AppUnauth changeAuth={bool => setAuthenticated(bool)}/>);
+}
+
 
 export default App;
