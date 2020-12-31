9a5ea40a5efc4338f0a28e6e55964e93658f5030
Kosa103 <dotomiro2@gmail.com>
2020-12-31 10:53:47 +0100

    added input handling

--- src/AppAuthComponents.js
+++ src/AppAuthComponents.js
@@ -2,6 +2,36 @@ import React from "react";
 
 
 export function ContentAddVehicles() {
+    const defaultFormField = {
+        type: "text",
+        size: "30",
+        maxLength: "30",
+        className: "input input-text form-field"
+    };
+    const longFormField = {
+        type: "text",
+        size: "120",
+        maxLength: "120",
+        className: "input input-long-text form-field"
+    };
+    const dateFormField = {
+        type: "date",
+        size: "10",
+        maxLength: "10",
+        className: "input input-date form-field"
+    };
+    const numberFormField = {
+        type: "number",
+        size: "30",
+        maxLength: "30",
+        className: "input input-number form-field"
+    };
+    const commentsFormField = {
+        type: "text",
+        size: "100000",
+        maxLength: "100000",
+        className: "input input-comments form-field"
+    };
     const form = {
         registrationNumber: React.useRef(null),
         dateOfFirstRegistration: React.useRef(null),
@@ -27,86 +57,130 @@ export function ContentAddVehicles() {
         firstName: React.useRef(null),
         address: React.useRef(null),
         ownershipType: React.useRef(null),
+        examValidUntil: React.useRef(null),
         comments: React.useRef(null),
     };
+    const postAlert = React.useRef(null);
 
-    function attemptPostForm() {
+    function sanitizeInput(input) {
+        const stringInput = input.toString();
+        return stringInput.replace("(", "").replace(")", "").replace("{", "").replace("}", "").replace("*", "").replace(";", "").replace("%", "").toUpperCase();
+    }
+
+    function removeDash(input) {
+        return input.replace("-", "");
+    }
+
+    async function attemptPostForm() {
         const values = {
-            registrationNumber: form.registrationNumber.current.value,
-            dateOfFirstRegistration: form.dateOfFirstRegistration.current.value,
-            brand: form.brand.current.value,
-            type: form.type.current.value,
-            model: form.model.current.value,
-            displacement: form.displacement.current.value,
-            engineCode: form.engineCode.current.value,
-            eurotaxCode: form.eurotaxCode.current.value,
-            enginePower: form.enginePower.current.value,
-            chassisNumber: form.chassisNumber.current.value,
-            fuelType: form.fuelType.current.value,
-            vehicleCategory: form.vehicleCategory.current.value,
-            emptyWeight: form.emptyWeight.current.value,
-            maximumWeight: form.maximumWeight.current.value,
-            typeAcknowledgementNumber: form.typeAcknowledgementNumber.current.value,
-            dateOfFirstRegistrationInHungary: form.dateOfFirstRegistrationInHungary.current.value,
-            yearOfManufacturing: form.yearOfManufacturing.current.value,
-            color: form.color.current.value,
-            environmentalClass: form.environmentalClass.current.value,
-            numberOfSeats: form.numberOfSeats.current.value,
-            lastNameOrCompanyName: form.lastNameOrCompanyName.current.value,
-            firstName: form.firstName.current.value,
-            address: form.address.current.value,
-            ownershipType: form.ownershipType.current.value,
-            comments: form.comments.current.value,
+            registrationNumber: sanitizeInput(removeDash(form.registrationNumber.current.value)),
+            dateOfFirstRegistration: sanitizeInput(form.dateOfFirstRegistration.current.value),
+            brand: sanitizeInput(form.brand.current.value),
+            type: sanitizeInput(form.type.current.value),
+            model: sanitizeInput(form.model.current.value),
+            displacement: sanitizeInput(form.displacement.current.value),
+            engineCode: sanitizeInput(form.engineCode.current.value),
+            eurotaxCode: sanitizeInput(form.eurotaxCode.current.value),
+            enginePower: sanitizeInput(form.enginePower.current.value),
+            chassisNumber: sanitizeInput(form.chassisNumber.current.value),
+            fuelType: sanitizeInput(form.fuelType.current.value),
+            vehicleCategory: sanitizeInput(form.vehicleCategory.current.value),
+            emptyWeight: sanitizeInput(form.emptyWeight.current.value),
+            maximumWeight: sanitizeInput(form.maximumWeight.current.value),
+            typeAcknowledgementNumber: sanitizeInput(form.typeAcknowledgementNumber.current.value),
+            dateOfFirstRegistrationInHungary: sanitizeInput(form.dateOfFirstRegistrationInHungary.current.value),
+            yearOfManufacturing: sanitizeInput(form.yearOfManufacturing.current.value),
+            color: sanitizeInput(form.color.current.value),
+            environmentalClass: sanitizeInput(form.environmentalClass.current.value),
+            numberOfSeats: sanitizeInput(form.numberOfSeats.current.value),
+            lastNameOrCompanyName: sanitizeInput(form.lastNameOrCompanyName.current.value),
+            firstName: sanitizeInput(form.firstName.current.value),
+            address: sanitizeInput(form.address.current.value),
+            ownershipType: sanitizeInput(form.ownershipType.current.value),
+            examValidUntil: sanitizeInput(form.examValidUntil.current.value),
+            comments: sanitizeInput(form.comments.current.value),
         };
+        const requestBody = {};
+        for (const [key, value] of Object.entries(values)) {
+            if (value) {
+                requestBody[key] = value;
+            }
+        }
+        const token = JSON.parse(sessionStorage.getItem("res")).jwt;
+        const url = "http://borzalom.ddns.net:1000/vehicles";
+        const options = {
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json',
+                Authorization : `Bearer ${token}`
+            },
+            body: JSON.stringify(requestBody)
+        };
+        let responseStatus = 0;
 
-        console.log(values);
+        const response = await fetch(url, options)
+        .then(res => {
+            responseStatus = res.status;
+            return res.json();
+        })
+        .catch(err => console.log(err));
+
+        if (responseStatus >= 200 && responseStatus < 300) {
+            postAlert.current.textContent = "Successfully added vehicle";
+        } else {
+            postAlert.current.textContent = "Error adding vehicle!"
+            console.log("Error code:");
+            console.log(responseStatus);
+        }
     }
 
     return (
         <div className="content-box form-box">
             <h2>Add new vehicle:</h2>
             <p>Fields marked with an asterisk (*) are mandatory to fill!</p>
+            <p ref={postAlert}></p>
             <div className="form-page">
                 <div className="form-subpage">
-                    <FormField title="[A] Registration number*:" reference={form.registrationNumber}/>
-                    <FormField title="[B] Date of first registration*:" reference={form.dateOfFirstRegistration}/>
-                    <FormField title="[D.1] Brand*:" reference={form.brand}/>
-                    <FormField title="[D.2] Type:" reference={form.type}/>
-                    <FormField title="[D.3] Model*:" reference={form.model}/>
-                    <FormField title="[P.1] Displacement*:" reference={form.displacement}/>
-                    <FormField title="[P.5] Engine code*:" reference={form.engineCode}/>
+                    <FormField title="[A] Registration number*:" properties={defaultFormField} reference={form.registrationNumber}/>
+                    <FormField title="[B] Date of first registration*:" properties={dateFormField} reference={form.dateOfFirstRegistration}/>
+                    <FormField title="[D.1] Brand*:" properties={defaultFormField} reference={form.brand}/>
+                    <FormField title="[D.2] Type:" properties={defaultFormField} reference={form.type}/>
+                    <FormField title="[D.3] Model*:" properties={defaultFormField} reference={form.model}/>
+                    <FormField title="[P.1] Displacement*:" properties={numberFormField} reference={form.displacement}/>
+                    <FormField title="[P.5] Engine code*:" properties={defaultFormField} reference={form.engineCode}/>
                 </div>
                 <div className="form-subpage">
-                    <FormField title="[0] Eurotax code:" reference={form.eurotaxCode}/>
-                    <FormField title="[P.2] Engine power*:" reference={form.enginePower}/>
-                    <FormField title="[E] Chassis number*:" reference={form.chassisNumber}/>
-                    <FormField title="[P.3] Fuel type*:" reference={form.fuelType}/>
-                    <FormField title="[J] Vehicle category:" reference={form.vehicleCategory}/>
+                    <FormField title="[0] Eurotax code:" properties={defaultFormField} reference={form.eurotaxCode}/>
+                    <FormField title="[P.2] Engine power*:" properties={numberFormField} reference={form.enginePower}/>
+                    <FormField title="[E] Chassis number*:" properties={defaultFormField} reference={form.chassisNumber}/>
+                    <FormField title="[P.3] Fuel type*:" properties={defaultFormField} reference={form.fuelType}/>
+                    <FormField title="[J] Vehicle category*:" properties={defaultFormField} reference={form.vehicleCategory}/>
                 </div>
             </div>
             <div className="form-page">
                 <div className="form-subpage">
-                    <FormField title="[G] Empty Weight:" reference={form.emptyWeight}/>
-                    <FormField title="[F.1] Maximum Weight:" reference={form.maximumWeight}/>
-                    <FormField title="[K] Type acknowledgement number:" reference={form.typeAcknowledgementNumber}/>
-                    <FormField title="[I] Date of first registration in hungary:" reference={form.dateOfFirstRegistrationInHungary}/>
-                    <FormField title="[00] Year of manufacturing:" reference={form.yearOfManufacturing}/>
+                    <FormField title="[G] Empty Weight:" properties={numberFormField} reference={form.emptyWeight}/>
+                    <FormField title="[F.1] Maximum Weight:" properties={numberFormField} reference={form.maximumWeight}/>
+                    <FormField title="[K] Type acknowledgement number:" properties={defaultFormField} reference={form.typeAcknowledgementNumber}/>
+                    <FormField title="[I] Date of first registration in hungary:" properties={dateFormField} reference={form.dateOfFirstRegistrationInHungary}/>
+                    <FormField title="[00] Year of manufacturing:" properties={numberFormField} reference={form.yearOfManufacturing}/>
                 </div>
                 <div className="form-subpage">
-                    <FormField title="[R] Color:" reference={form.color}/>
-                    <FormField title="[V.9] Environmental class:" reference={form.environmentalClass}/>
-                    <FormField title="[S.1] Number of seats:" reference={form.numberOfSeats}/>
+                    <FormField title="[R] Color:" properties={defaultFormField} reference={form.color}/>
+                    <FormField title="[V.9] Environmental class:" properties={defaultFormField} reference={form.environmentalClass}/>
+                    <FormField title="[S.1] Number of seats:" properties={numberFormField} reference={form.numberOfSeats}/>
                 </div>
             </div>
             <div className="form-page">
                 <div className="form-subpage">
-                    <FormField title="[C.1.1] Last name or company name:" reference={form.lastNameOrCompanyName}/>
-                    <FormField title="[C.1.2] First name:" reference={form.firstName}/>
-                    <FormField title="[C.1.3] Address:" reference={form.address}/>
-                    <FormField title="[C.1.4] Ownership type:" reference={form.ownershipType}/>
+                    <FormField title="[C.1.1] Last name or company name:" properties={defaultFormField} reference={form.lastNameOrCompanyName}/>
+                    <FormField title="[C.1.2] First name:" properties={defaultFormField} reference={form.firstName}/>
+                    <FormField title="[C.1.3] Address:" properties={defaultFormField} reference={form.address}/>
+                    <FormField title="[C.1.4] Ownership type:" properties={defaultFormField} reference={form.ownershipType}/>
+                    <FormField title="[000] Exam valid until:" properties={dateFormField} reference={form.examValidUntil}/>
                 </div>
                 <div className="form-subpage">
-                    <FormField title="[000] Comments:" reference={form.comments}/>
+                    <FormField title="[0000] Comments:" properties={commentsFormField} reference={form.comments}/>
                 </div>
             </div>
             <button className="button form-submit-button" onClick={() => attemptPostForm()}>Add vehicle</button>
@@ -117,13 +191,15 @@ export function ContentAddVehicles() {
 
 function FormField(props) {
     const title = props.title;
+    const {type, size, maxLength, className} = {...props.properties};
     const reference = props.reference;
+
     const id = title.split("] ")[1].toLowerCase().replace(" ", "-").replace(":", "").replace("*", "");
 
     return (
         <div className="form-field">
             <p>{title}</p>
-            <input type="text" size="40" maxLength="40" className="input form-field" id={`input-${id}`} ref={reference}/>
+            <input type={type} size={size} maxLength={maxLength} className={className} id={`input-${id}`} ref={reference}/>
         </div>
     );
 }
